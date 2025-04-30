48eb3a7cb7299c1b618b0a3e9b6da48ff9daf8b4
xrishx <69658498+xrishx@users.noreply.github.com>
2025-04-30 17:32:57 +0545

    Create python_syntax.py
    Python syntax including data types, control structures and functions

---
+++ python_syntax.py
@@ -0,0 +1,33 @@
+# Basic Data Types
+age = 12                # int
+height = 5.9            # float
+name = "Alice"          # string
+is_student = True       # boolean
+
+# Control Structures
+
+# Conditionals
+if age > 18:
+    print("Adult")
+elif age == 18:
+    print("Just became an adult")
+else:
+    print("Minor")
+
+# For Loop
+for i in range(5):      # iterates from 0 to 4
+    print(f"Number: {i}")
+
+# While loop
+count = 0 
+while count < 0:
+    print("Counting:", count)
+    count += 1
+
+# Functions
+def greet(name):
+    return f"Hello, {name}!"
+
+# Function Call
+print(greet(name))
+
