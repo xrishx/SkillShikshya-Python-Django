9641b3188f1faec543b657b83056cb6fe65ece7d
xrishx <69658498+xrishx@users.noreply.github.com>
2025-05-07 15:56:20 +0545

    Create bitwise_operators.py
    bitwise operators

---
+++ Python-Operators/bitwise_operators.py
@@ -0,0 +1,25 @@
+x = 10
+y = 4
+
+# AND Operator
+print("x & y = ", x & y)
+
+# OR Operator
+print("x | y = ", x | y)
+
+# XOR Operator
+print("x ^ y = ", x ^ y)
+
+# NOT Operator
+print(" ~x = ", ~ x)
+
+
+#---Bitwise Shift---
+
+# Left Shift
+print("x << 1 =", x << 1)
+print("y << 1 =", y << 1)
+
+# Right Shift
+print("x >> 1 =", x >> 1)
+print("y >> 1 =", y >> 1)
