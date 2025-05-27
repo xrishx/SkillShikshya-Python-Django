c4574baeb5571ec888ade44d7022a53b321c364d
Rishav Shrestha <69658498+xrishx@users.noreply.github.com>
2025-05-27 18:12:22 +0545

    nested dict

--- Collections/DICTIONARIES/dictionaries_methods.py
+++ Collections/DICTIONARIES/dictionaries_methods.py
@@ -1,3 +1,5 @@
+import copy
+
 this_dict = {"name":"Rishav", "age":24 , "year":2000}
 
 # Update the item by referring it's key name
@@ -39,9 +41,51 @@ print(new_dict)
 
     # copy() method
 # copy the dict
-new_dict = new_dict.copy()
+news_dict = new_dict.copy()
 # also can do
-new_dict = dict(new_dict)
+news_dict = dict(new_dict)
 
 # Shallow copy changes the original list as well
 # Deep copy doesn't change the original list when edited
+deepcopy_newdict = copy.deepcopy(new_dict) # have to import copy
+
+
+        # Nested Dictionaires
+# Dictionary inside a dictionary
+
+# Direct Method
+nested_dict = {
+    "Fruits": {"Yummy": "apple", "Yucky": "guava"},
+    "Flower": {"Like": "tulip", "loathe": "rose"}
+}   
+print(nested_dict)
+
+# Using Loop
+nested_dict = {}
+parameters = ["Fruits", "Colors"]
+for key in parameters:
+    nested_dict[key] = {"Yummy": "Orange", "Lovely": "blueberry"}
+print(nested_dict)
+
+
+## Adding to Nested Dict
+# Adding a new key-value pair to Fruits's nested dictionary
+nested_dict["Fruits"]["Like"] = "Blackberry"
+print(nested_dict)
+# Adding new nested dict
+nested_dict["Sports"] = {"Can play": "Football", "CAnt play": "Basketball"}
+print(nested_dict)
+
+## Accessing in Nested Dict
+# Direct Indexing
+yummy_fruits = nested_dict["Fruits"]["Yummy"]
+print("Yummy fruit is:", yummy_fruits)
+# Using get() method
+loathe_flower = nested_dict.get("Flower", {}).get("loathe", "Not Found") 
+# returns Not Found if doesnt exist instead of error
+print("Loathe Flower:", loathe_flower)  
+
+## Deleting in Nested Dict
+# use del
+del nested_dict["Sports"]
+print(nested_dict)
